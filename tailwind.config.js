8fca4b08cb07d9a6a1a6d0395e6d808215aca4fe
Genesis <genesismaraya6@gmail.com>
2025-10-16 21:47:48 +0800

    Adding first layout

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    }
+  },
+  plugins: [],
+}
